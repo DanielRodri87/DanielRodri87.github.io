787d5e919d741c7fcc1355c1400fd4a5bbb17b08
Daniel Rodrigues <86251321+DanielRodri87@users.noreply.github.com>
2022-07-04 20:33:27 -0300

    site

---
+++ script.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("enviar");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) os seus dados foram encaminhados com sucesso');
+    }
+
+}
