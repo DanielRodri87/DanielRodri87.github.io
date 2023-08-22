70040a7ce5142a645a8ed33760b0e66a3acdbac8
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-08-22 13:13:06 -0300

    att

--- script.js
+++
@@ -1,9 +0,0 @@
-function Enviar() {
-
-    var nome = document.getElementById("enviar");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) os seus dados foram encaminhados com sucesso');
-    }
-
-}
